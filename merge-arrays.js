0812b0d22fb165376ecadefa505a1062eb7dee05
Brandon Macdonald <bmacdonald1986@gmail.com>
2021-03-06 23:18:13 +0000

    Completed Stretch Work

---
+++ merge-arrays.js
@@ -0,0 +1,9 @@
+function merge(arr1, arr2){
+  let mArr = [];
+  mArr = arr1.concat(arr2);
+  return(mArr.sort());
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
